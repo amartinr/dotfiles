3d4c1c8620ef76f0a59d29534aa27ed584ece774
Abel Martín <abel.martin.ruiz@gmail.com>
2017-09-13 20:00:27 +0200

    Make i3 IPC workspace renaming script use bold typeface for current workspace name

--- .scripts/i3/i3ipc_rename_workspace.py
+++ .scripts/i3/i3ipc_rename_workspace.py
@@ -5,38 +5,43 @@ import sys
 
 i3 = i3ipc.Connection()
 
-def rename_current_workspace(i3, e):
+def add_title_to_ws_name(i3, e):
     focused_workspace = i3.get_tree().find_focused().workspace()
     focused_window = i3.get_tree().find_focused()
+
+    #ws_name =  "%s: %s" % (focused_workspace.num, focused_window.name)
+    ws_num =  focused_workspace.num
+    ws_name = focused_window.name
+
+    max_length = 72
+    if len(ws_name) > max_length:
+        ws_name = '{}...'.format(ws_name[:max_length-3])
+
+    ## bold
+    i3.command('rename workspace to "%s:<b>%s: %s</b>"' % (ws_num, ws_num, ws_name))
+    ## regular
+    #i3.command('rename workspace to "%s:%s: %s"' % (ws_num, ws_num, ws_name))
 
     print("-----------------------")
     print()
     print("focused workspace number: %s" % focused_workspace.num)
     print("focused workspace name: %s" % focused_workspace.name)
     print("focused window name: %s" % focused_window.name)
-    
-    ws_name =  "%s: %s" % (focused_workspace.num, focused_window.name)
-    num = focused_workspace.num
-
-    if len(ws_name) > 72:
-        ws_name = '{}...'.format(ws_name[:69])
-
     print("ws_name: %s" % ws_name)
     print()
-   
-    i3.command('rename workspace to "%s"' % ws_name)
 
 def clear_current_workspace(i3, e):
     focused_workspace = i3.get_tree().find_focused().workspace()
-    i3.command('rename workspace to "%s"' % focused_workspace.num)
+    i3.command('rename workspace to "%s:<b>%s</b>"' % (focused_workspace.num, focused_workspace.num))
 
-def rename_old_workspace(i3, e):
-    i3.command('rename workspace "%s" to "%s"' % (e.old.name, e.old.num))
+def rename_prev_workspace(i3, e):
+    i3.command('rename workspace "%s" to "%s:%s"' % (e.old.name, e.old.num, e.old.num))
+    i3.command('rename workspace "%s" to "%s:<b>%s</b>"' % (e.current.name, e.current.num, e.current.num))
 
-i3.on('window::focus', rename_current_workspace)
-i3.on('window::title', rename_current_workspace)
+i3.on('window::focus', add_title_to_ws_name)
+i3.on('window::title', add_title_to_ws_name)
 i3.on('window::move', clear_current_workspace)
 i3.on('window::close', clear_current_workspace)
-i3.on('workspace::focus', rename_old_workspace)
+i3.on('workspace::focus', rename_prev_workspace)
 
 i3.main()
