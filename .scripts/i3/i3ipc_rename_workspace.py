c358994c14e95f45caa566a178c402b715f028bb
Abel Martín <abel.martin.ruiz@gmail.com>
2018-04-04 23:07:43 +0200

    Escape pango characters

--- .scripts/i3/i3ipc_rename_workspace.py
+++ .scripts/i3/i3ipc_rename_workspace.py
@@ -1,90 +1,92 @@
 #!/usr/bin/env python3
 
 import i3ipc
+from html import escape
+
+def format_string(string):
+    max_length = 72
+
+    if len(string) > max_length:
+        string = '{}...'.format(string[:max_length-3])
+
+    return '<b>{0}</b>'.format(escape(string))
 
 def rename_workspace(i3, e):
+
     focused_window = i3.get_tree().find_focused()
+
+    if focused_window.name is None:
+        return
+
     focused_workspace = focused_window.workspace()
 
     ws_num = focused_workspace.num
     window_name = focused_window.name
-
-    max_length = 72
-    if len(window_name) > max_length:
-        window_name = '{}...'.format(window_name[:max_length-3]).replace('"',
-                                                                         r'\"')
-    else:
-        window_name = window_name.replace('"', r'\"')
 
     if focused_window.parent.layout in ('tabbed', 'stacked'):
         if len(focused_window.parent.nodes) > 1:
-            i3.command('rename workspace to "%s:<b>%s</b>"' % (ws_num, ws_num))
+            ws_name = format_string(str(ws_num))
         else:
-            i3.command('rename workspace to "%s:<b>%s: %s</b>"' % (ws_num,
-                                                                   ws_num,
-                                                                   window_name))
+            ws_name = format_string(str(ws_num) + ': ' + window_name)
     else:
-        i3.command('rename workspace to "%s:<b>%s: %s</b>"' % (ws_num,
-                                                               ws_num,
-                                                               window_name))
+            ws_name = format_string(str(ws_num) + ': ' + window_name)
+
+    i3.command('rename workspace to "%s:%s"' % (ws_num, ws_name))
 
 def clear_workspace(i3, e):
+    print("clear_workspace")
     focused_window = i3.get_tree().find_focused()
     focused_workspace = focused_window.workspace()
 
     ws_num = focused_workspace.num
     window_name = focused_window.name
 
-    max_length = 72
-    if len(window_name) > max_length:
-        window_name = '{}...'.format(window_name[:max_length-3]).replace('"',
-                                                                         r'\"')
-    else:
-        window_name = window_name.replace('"', r'\"')
-
     if len(focused_workspace.nodes) == 0:
-        i3.command('rename workspace to "%s:<b>%s</b>"' % (ws_num, ws_num))
+        i3.command('rename workspace to "%s:%s"' % (ws_num, format_string(str(ws_num))))
     else:
         if focused_window.parent.layout in ('tabbed', 'stacked'):
-            i3.command('rename workspace to "%s:<b>%s</b>"' % (ws_num, ws_num))
+            i3.command('rename workspace to "%s:%s"' % (ws_num, format_string(str(ws_num))))
 
         elif focused_window.parent.layout in ('splith', 'splitv'):
-            i3.command('rename workspace to "%s:<b>%s: %s</b>"' % (ws_num, ws_num, window_name))
+            i3.command('rename workspace to "%s:%s"' % (ws_num, format_string(str(ws_num) + ': ' + window_name)))
 
 
 def focus_workspace(i3, e):
+    print("{0} - old name: {1}".format("focus_workspace", e.old.name))
+    # old workspace
     i3.command('rename workspace "%s" to "%s:%s"' % (e.old.name, e.old.num, e.old.num))
-    i3.command('rename workspace "%s" to "%s:<b>%s</b>"' % (e.current.name, e.current.num, e.current.num))
+    # new workspace
+    i3.command('rename workspace to "%s:%s"' % (e.current.num, format_string(str(e.current.num))))
 
 
 def layout_change(i3, e):
     focused_window = i3.get_tree().find_focused()
     focused_workspace = focused_window.workspace()
 
+    if focused_window.name is None:
+        return
+
     ws_num = focused_workspace.num
     window_name = focused_window.name
-
-    max_length = 72
-    if len(window_name) > max_length:
-        window_name = '{}...'.format(window_name[:max_length-3]).replace('"',
-                                                                         r'\"')
-    else:
-        window_name = window_name.replace('"', r'\"')
 
     command = e.binding.command.split(None, 1)
 
     if command[0] == 'layout':
+        print("layout_change")
         layout = command[1]
         if layout in ('tabbed', 'stacking', 'stacked'):
             if len(focused_window.parent.nodes) > 1:
-                i3.command('rename workspace to "%s:<b>%s</b>"' % (ws_num, ws_num))
+                i3.command('rename workspace to "%s:%s"' % (ws_num, format_string(str(ws_num))))
             else:
-                i3.command('rename workspace to "%s:<b>%s: %s</b>"' % (ws_num, ws_num, window_name))
+                i3.command('rename workspace to "%s:%s"' % (ws_num, format_string(str(ws_num) + ': ' + window_name)))
         elif layout in ('toggle split', 'splith', 'splitv'):
-            i3.command('rename workspace to "%s:<b>%s: %s</b>"' % (ws_num, ws_num, window_name))
+            i3.command('rename workspace to "%s:%s"' % (ws_num, format_string(str(ws_num) + ': ' + window_name)))
     elif command[0] == 'split':
-        i3.command('rename workspace to "%s:<b>%s: %s</b>"' % (ws_num, ws_num, window_name))
+        print("layout_change")
+        i3.command('rename workspace to "%s:%s"' % (ws_num, format_string(str(ws_num) + ': ' + window_name)))
 
+    else:
+        print("command: {0}".format(command[0]))
 
 i3 = i3ipc.Connection()
 
