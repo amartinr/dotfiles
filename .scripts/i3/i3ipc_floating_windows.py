8b910208fe0e6be525a8867e1551bc9848d9c0eb
Abel Martín <abel.martin.ruiz@gmail.com>
2019-02-14 08:43:47 +0100

    Prevent new browser child windows from floating upon creation

--- .scripts/i3/i3ipc_floating_windows.py
+++ .scripts/i3/i3ipc_floating_windows.py
@@ -7,8 +7,9 @@ class_terminals = {'URxvt', 'Terminator', 'Termite', 'Xfce4-terminal'}
 class_editors = {'Mousepad'}
 class_browsers = {'Firefox-esr', 'Chromium', 'Firefox'}
 
-# Make new children windows of existing ones floating except for editors, terminals
-# and browsers
+
+# Make new children windows of existing ones floating except for editors,
+# terminals and browsers
 def on_window_new(i3, event):
     ws = i3.get_tree().find_focused().workspace()
     leaves = ws.leaves()
@@ -17,21 +18,19 @@ def on_window_new(i3, event):
 
     width = 1360
     height = 920
- 
+
     if event.container.window_class not in class_terminals | class_editors:
-    #if event.container.window_class not in class_terminals | class_editors | \
-    #                                       class_browsers:
         for leaf in leaves:
             if event.container.window_class in leaf.window_class:
                 wc_count = wc_count + 1
 
         if wc_count > 1:
-           resize_command = 'resize set {0} {1}'.format(width, height)
+            resize_command = 'resize set {0} {1}'.format(width, height)
 
-           event.container.command('floating toggle')
-           event.container.command(resize_command)
-           event.container.command('move position center')
-            
+            event.container.command('floating toggle')
+            event.container.command(resize_command)
+            event.container.command('move position center')
+
 
 # Subscribe to events
 i3.on("window::new", on_window_new)
