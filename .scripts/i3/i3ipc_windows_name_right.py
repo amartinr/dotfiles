644594c3f559e3d986b84391be74c7fc9bb2b3fe
Abel Martín <abel.martin.ruiz@gmail.com>
2019-02-14 16:00:48 +0100

    Fix layout change behaviour

--- .scripts/i3/i3ipc_windows_name_right.py
+++ .scripts/i3/i3ipc_windows_name_right.py
@@ -31,21 +31,24 @@ def update_window_name(i3, e):
 
 def layout_change(i3, e):
     focused_window = i3.get_tree().find_focused()
-    # focused_workspace = focused_window.workspace()
+    focused_workspace = focused_window.workspace()
 
     command = e.binding.command.split(None, 1)
 
-    if command[0] == 'layout':
-        layout = command[1]
-        if layout in ('tabbed', 'stacking', 'stacked'):
-            if len(focused_window.parent.nodes) > 1:
-                print()
-            else:
+    if focused_workspace.num >= 4:
+        if command[0] == 'layout':
+            layout = command[1]
+            if layout in ('tabbed', 'stacking', 'stacked'):
+                if len(focused_window.parent.nodes) > 1:
+                    print()
+                else:
+                    print(focused_window.name)
+            elif layout in ('toggle split', 'splith', 'splitv'):
                 print(focused_window.name)
-        elif layout in ('toggle split', 'splith', 'splitv'):
+        elif command[0] == 'split':
             print(focused_window.name)
-    elif command[0] == 'split':
-        print(focused_window.name)
+    else:
+        print()
 
 
 i3.on('window::focus', update_window_name)
